1f20d227f19ca2911a6d1fd948ba1843dd3dd91f
Un nombre <tu@mail.com>
2024-05-17 21:19:17 +0200

    cerrando para hacer pull a la develop

--- src/main/java/es/etg/programacion/mercadaw/dao/MercadoMariadbDAOImp.java
+++ src/main/java/es/etg/programacion/mercadaw/dao/MercadoMariadbDAOImp.java
@@ -2,6 +2,10 @@ package es.etg.programacion.mercadaw.dao;
 
 import java.io.File;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import es.etg.programacion.mercadaw.producto.Producto;
@@ -10,16 +14,35 @@ import es.etg.programacion.mercadaw.trabajador.Empleado;
 public class MercadoMariadbDAOImp implements MercadoDAO{
     private Connection conn;
     private final String URL = "jdbc:mariadb://localhost:3306/MercaDAW";
+    private final String DATABASE_NAME = "MercaDAW";
     private final String DATABASE_USER = "SYSTEM";
     private final String DATABASE_PASS = "secret";
-    public MercadoMariadbDAOImp(){
-        
+    public MercadoMariadbDAOImp() throws Exception{
+        conn = DriverManager.getConnection(String.format(URL, DATABASE_NAME, DATABASE_USER, DATABASE_PASS));
         }
     
     @Override
     public List<Empleado> listarEmpleados() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listarEmpleados'");
+
+        final String query = "SELECT id, nombre, apellidos, categoria FROM empleados";
+
+        List<Empleado> empleados = new ArrayList<Empleado>();
+        PreparedStatement ps = conn.prepareStatement(query);
+        ResultSet rs = ps.executeQuery();
+
+        while(rs.next()){
+            int id = rs.getInt("id");
+            String nombre = rs.getString("nombre");
+            String apellidos = rs.getString("apellidos");
+            String categoria = rs.getString("categoria");
+
+            Empleado a = new Empleado(id, nombre, apellidos, categoria);
+            empleados.add(a);
+        }
+        rs.close();
+        ps.close();
+
+        return empleados;
     }
 
     @Override

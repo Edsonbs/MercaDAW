a4a85cd16d690d25b6e8942fe3d1edd1a23ee3a7
EddyCode <eddysmail5bs@gmail.com>
2024-05-17 14:35:42 +0200

    JUnit 5.11.0-M1 añadido al POM y la carpeta test con un ejemplo.

---
+++ src/test/java/es/etg/mercadaw/AppTest.java
@@ -0,0 +1,17 @@
+package es.etg.mercadaw;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest 
+{
+    /**
+     * Test extremadamente riguroso :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue(true);
+    }
+}

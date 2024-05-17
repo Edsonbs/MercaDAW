0d6c7a54de381ffe79f0b27f70b7bc32ddba3db6
Un nombre <tu@mail.com>
2024-05-17 21:34:42 +0200

    me acabo de dar cuenta que estoy en develop

--- src/main/java/es/etg/programacion/mercadaw/dao/MercadoMariadbDAOImp.java
+++ src/main/java/es/etg/programacion/mercadaw/dao/MercadoMariadbDAOImp.java
@@ -47,8 +47,25 @@ public class MercadoMariadbDAOImp implements MercadoDAO{
 
     @Override
     public List<Producto> listarProductos() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listarProductos'");
+        final String query = "SELECT nombre, marca, categoria, precio_en_euros, iva, altura_en_metros, anchura_en_metros, peso_en_kg, numero_elementos, descripcion FROM productos";
+
+        List<Producto> productos = new ArrayList<Producto>();
+        PreparedStatement ps = conn.prepareStatement(query);
+        ResultSet rs = ps.executeQuery();
+
+        while(rs.next()){
+            int id = rs.getInt("id");
+            String nombre = rs.getString("nombre");
+            String apellidos = rs.getString("marca");
+            String categoria = rs.getString("categoria");
+
+            Empleado a = new Empleado(id, nombre, apellidos, categoria);
+            empleados.add(a);
+        }
+        rs.close();
+        ps.close();
+
+        return empleados;
     }
 
     @Override
